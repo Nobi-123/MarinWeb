3888625cfa74ce7ac12ca10db0a4ee7fc936510d
Shadow737hub <nothinnothin162@gmail.com>
2025-08-27 17:34:42 +0530

    Update logo.jsx

--- components/page/logo.jsx
+++ components/page/logo.jsx
@@ -4,7 +4,7 @@ export default function Logo() {
     return (
         <Link href="/" className="select-none">
             <div>
-                <h1 className="text-2xl font-bold">Wave<span className="opacity-50">Tune</span></h1>
+                <h1 className="text-2xl font-bold">- `ᴍʏʀᴀ ✗ <span className="opacity-50">𝐌ᴜѕɪᴄ's</span></h1>
             </div>
         </Link>
     )
